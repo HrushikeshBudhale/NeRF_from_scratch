896d18cf8bba6912241e7ee488787c84cf382fb2
Hrushikesh <hruhnb@gmail.com>
2024-09-21 19:11:14 +0000

    update sampling to sample around middle of segment

--- ray_sampling.py
+++ ray_sampling.py
@@ -14,8 +14,10 @@ class RaySampler:
         # r_d: (N_rays, 3)
         z_vals = torch.linspace(self.near, self.far, self.N_samples, device=r_o.device)                         # (N_samples)
         z_vals = z_vals.unsqueeze(0).repeat(r_o.shape[0], 1)                                                    # (N_rays, N_samples)
-        if self.perturb_samples:
-            z_vals += torch.rand(r_o.shape[0], self.N_samples, device=r_o.device) * self.step_size
+        mids = (z_vals[:, 1:] + z_vals[:, :-1]) / 2.0                                                           # (N_rays, N_samples-1)
+        lower = torch.cat([z_vals[:, :1], mids], dim=-1)                                                        # (N_rays, N_samples)
+        upper = torch.cat([mids, z_vals[:, -1:]], dim=-1)                                                       # (N_rays, N_samples)
+        z_vals = lower + (upper - lower) * torch.rand(z_vals.shape, device=r_o.device) * self.perturb_samples   # (N_rays, N_samples)
         r_o, r_d = r_o.unsqueeze(1).repeat(1, self.N_samples, 1), r_d.unsqueeze(1).repeat(1, self.N_samples, 1) # (N_rays, N_samples, 3), (N_rays, N_samples, 3)
         points = r_o + r_d * z_vals.unsqueeze(-1)                                                               # (N_rays, N_samples, 3)
         return points, z_vals
