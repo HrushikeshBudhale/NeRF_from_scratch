f71d80ee43494a86811a86bf675a95fd9c835cc2
Hrushikesh <hruhnb@gmail.com>
2024-09-21 19:11:58 +0000

    add shape comments

--- model.py
+++ model.py
@@ -43,18 +43,18 @@ class Nerf(nn.Module):
 
     def positional_encoding(self, x: torch.Tensor, L: int):
         # x: (N_rays, N_samples, 3)
-        freqs = 2.0 ** torch.arange(L, dtype=x.dtype).to(x.device)
-        x_input = x.unsqueeze(-1) * freqs * 2 * torch.pi
-        encoding = torch.cat([torch.sin(x_input), torch.cos(x_input)], dim=-1)
-        encoding = torch.cat([x, encoding.reshape(*x.shape[:-1], -1)], dim=-1)
+        freqs = 2.0 ** torch.arange(L, dtype=x.dtype).to(x.device)                      # (L)
+        x_input = x.unsqueeze(-1) * freqs * 2 * torch.pi                                # (N_rays, N_samples, 3, L)
+        encoding = torch.cat([torch.sin(x_input), torch.cos(x_input)], dim=-1)          # (N_rays, N_samples, 3, L*2)
+        encoding = torch.cat([x, encoding.reshape(*x.shape[:-1], -1)], dim=-1)          # (N_rays, N_samples, 3+L*2*3)
         return encoding
 
     def forward(self, x, r_d):
         # x: (N_rays, N_samples, 3)
         # r_d: (N_rays, N_samples, 3)
         
-        x_encoded = self.positional_encoding(x, L=10) # (N_rays, N_samples, 2*3*10+3)
-        r_d_encoded = self.positional_encoding(r_d, L=4) # (N_rays, N_samples, 2*3*4+3)
+        x_encoded = self.positional_encoding(x, L=10)                                   # (N_rays, N_samples, 3+L*2*3)
+        r_d_encoded = self.positional_encoding(r_d, L=4)                                # (N_rays, N_samples, 3+L*2*3)
 
         x = self.relu(self.fc1_block1(x_encoded))
         x = self.relu(self.fc2_block1(x))
@@ -78,6 +78,6 @@ class Nerf(nn.Module):
         x = self.relu(self.fc2_rgb(x))
         rgb = self.sigmoid(self.fc3_rgb(x))
 
-        return rgb, density
+        return rgb, density                                                            # (N_rays, N_samples, 3), (N_rays, N_samples, 1)
     
 
